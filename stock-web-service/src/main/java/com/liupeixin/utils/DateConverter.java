44c2ae36028ea8dbdf521fba69ec8e0f0c8052d0
Peixin Liu <liupeixin1989@gmail.com>
2026-02-26 20:53:45 -0800

    update date converter

--- stock-web-service/src/main/java/com/liupeixin/utils/DateConverter.java
+++ stock-web-service/src/main/java/com/liupeixin/utils/DateConverter.java
@@ -4,9 +4,27 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class DateConverter {
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    public static String getGMTTime(Date externalDate) {
+        return convertGmt(externalDate).format(FORMATTER);
+    }
+
+    public static LocalDateTime convertGmt(Date externalDate) {
+        Instant instant = externalDate.toInstant();
+        ZonedDateTime gmtTime = instant.atZone(ZoneId.of("GMT"));
+        return gmtTime.toLocalDateTime();
+    }
+
+    public static String getNewYorkTime(Date externalDate) {
+        return convertGmtToNewYork(externalDate).format(FORMATTER);
+    }
+    
     public static LocalDateTime convertGmtToNewYork(Date date) {
         Instant instant = date.toInstant();
         ZonedDateTime gmtTime = instant.atZone(ZoneId.of("GMT"));
