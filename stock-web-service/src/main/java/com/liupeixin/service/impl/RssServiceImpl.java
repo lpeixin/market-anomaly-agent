a383aee6545a1b7fe022a6616ca8215c91cc2669
Peixin Liu <liupeixin1989@gmail.com>
2026-03-07 22:12:25 -0800

    optimize webpage crawling

--- stock-web-service/src/main/java/com/liupeixin/service/impl/RssServiceImpl.java
+++ stock-web-service/src/main/java/com/liupeixin/service/impl/RssServiceImpl.java
@@ -9,15 +9,12 @@ import com.rometools.rome.feed.synd.SyndEntry;
 import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.SyndFeedInput;
 import com.rometools.rome.io.XmlReader;
-import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Service;
 
 import java.net.URL;
 import java.time.LocalDateTime;
 import java.util.List;
-
-import static com.liupeixin.utils.StockInfoCrawler.getWebPage;
 
 @Service
 public class RssServiceImpl implements RssService {
@@ -27,8 +24,7 @@ public class RssServiceImpl implements RssService {
     @Override
     public void displayRssFeed() throws Exception {
         List<SyndEntry> rssList = this.fetchRssFeed(RSS_URL);
-        // TODO solve webpage crawling issue
-        // Document webPage = StockInfoCrawler.getWebPage();
+        Document webPage = StockInfoCrawler.getWebPage();
         if (rssList == null || rssList.isEmpty()) {
             System.out.println("RSS is empty.");
             return;
@@ -44,7 +40,7 @@ public class RssServiceImpl implements RssService {
             LocalDateTime cnDateTime = DateConverter.convertGmtToCn(entry.getPublishedDate());
             stockRss.setPubDateCn(cnDateTime);
             stockRss.setStockCode(getStockCode(title));
-            // stockRss.setTags(StockInfoCrawler.getTagsFromWebPage(actualTitle, webPage).toString());
+            stockRss.setTags(StockInfoCrawler.getTagsFromWebPage(actualTitle, webPage).toString());
             // TODO translate title to CN
             stockRss.setTitleCn("");
             System.out.println(stockRss.toString());
